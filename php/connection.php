8a91ea23d197aef9b6b677490306a37dddaaa800
DanielCalv <danielcalvert899@gmail.com>
2026-04-28 18:53:58 +0100

    modified

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "ibay");
+$conn = mysqli_connect("localhost", "group01", "Juvwusorn7rhFvEEdMaX", "group01");
 
 // Check connection
 if (!$conn) {
@@ -7,4 +7,4 @@ if (!$conn) {
 } else {
     echo "Successful Connection to MySQL";
 }
-?>
+?>php
